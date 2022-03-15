5a8712b938f15c05eeb3f00bd225fafd07e01424
searinox0902 <svquintana98@gmail.com>
2022-03-15 04:05:41 -0500

    finish frontend mentor challenge

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ['Outfit', 'sans-serif'],
+    },
+
+    extend: {
+      colors :{
+        white : 'hsl(0, 0%, 100%)',
+        black : '#000',
+        cTextBlue : 'hsl(218, 44%, 22%)',
+        cTextGray : ' hsl(220, 15%, 55%)',
+        cBlue : '#D5E1EF',
+      },
+    },
+  },
+  plugins: [],
+}
